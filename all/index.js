7786c19ca1f59ad2bcc81e692d481928d4e75cfa
Thomas Lebeau <lebeau.thomas@gmail.com>
2012-11-20 17:48:35 +0200

    Added install script

---
+++ all/index.js
@@ -0,0 +1,31 @@
+
+var util = require('util'),
+  yeoman = require('../../../../');
+
+
+module.exports = Generator;
+
+function Generator() {
+  yeoman.generators.Base.apply(this, arguments);
+}
+
+util.inherits(Generator, yeoman.generators.Base);
+
+Generator.prototype.main = function main() {
+  this.write('app/css/style.css', "/* Will be compiled down to a single stylesheet with your less files */");
+  this.directory('.', '.');
+};
+
+Generator.prototype.fetchBootstrap = function fetchBootstrap() {
+  var cb = this.async();
+
+  this.remote('twitter', 'bootstrap', 'master', function(err, remote) {
+    if(err) { return cb(err); }
+
+    remote.directory('less', 'app/less/bootstrap');
+    remote.directory('img', 'app/img');
+    remote.directory('js', 'app/js/vendor/bootstrap');
+
+    cb();
+  });
+};
---
+++ install.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+# Script to install the generator.
+# I am a novice in python so it's certainly not perfect. If you know a way to improve it, feel free to fork it
+
+generatorPath = '/usr/local/lib/node_modules/yeoman/node_modules/yeoman-generators/lib/generators'
+currentDir = os.path.dirname(os.path.abspath(__file__))
+bootstrapLessPath = os.path.join(generatorPath, 'bootstrap-less')
+
+# Check if Yeoman is installed
+if not os.path.isdir(generatorPath):
+    print 'It seems that Yeoman is not installed.'
+    print 'If you have trouble installing the generator please refer to the install instructions in the Readme.'
+    os.exit()
+
+# if the generator is already installed we remove it to avoid errors
+if os.path.isdir(bootstrapLessPath):
+    shutil.rmtree(bootstrapLessPath)
+try:
+    shutil.copytree(currentDir, bootstrapLessPath)
+    print '\033[32mGenerator installed successfully.\033[37m\n'
+except:
+    print 'Oops, something went wrong, sorry'
+    print 'If you have trouble installing the generator please refer to the install install instructions in the Readme.'
+    raise
