0df76e0b2446f393e2011d8def6d8364f2b3c8f1
Thomas Lebeau <lebeau.thomas@gmail.com>
2014-09-26 13:06:14 +0300

    fix task name

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     watch: {
       all: {
         files:['test/*.js', 'app/index.js'],
-        tasks:['mocha']
+        tasks:['mochaTest']
       }
     }
   });
